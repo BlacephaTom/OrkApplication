4ed70426c6fc9d0ac65f57196913d8cdc0dda9f0
Tom <tomgovier@live.co.uk>
2020-12-14 23:45:18 +0000

    Changed some view controls about, added some view models and collections to bind to, set up said binding

---
+++ OrkApplication/Structs/HQStructs.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrkApplication.Structs
+{
+    public class HQStructs
+    {
+
+        public struct HQUnit
+        {
+            public int PointsCost;
+            public string UnitName;
+        }
+
+        //public struct GhazghkullMagUrukThraka
+        //{
+        //    public const int PointsCost = 200;
+        //    public const string UnitName = "Ghazghkul Thraka";
+        //}
+
+
+    }
+
+    public class TroopsStructs
+    {
+
+
+    }
+
+}
---
+++ OrkApplication/ViewModels/ScreenViewModels/TotalsViewModel.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.ObjectModel;
+
+namespace OrkApplication.ViewModels.ScreenViewModels
+{
+    public class TotalsViewModel : BaseApplicationClasses.UpdateUIClass
+    {
+
+        public ObservableCollection<HQClass> HQColl { get; set; }
+        public ObservableCollection<TroopClass> TroopColl { get; set; }
+
+        public TotalsViewModel()
+        {
+            HQColl = new ObservableCollection<HQClass>() {
+                new HQClass("Thrakka", 189),
+                new HQClass("Boss", 45)
+            };
+
+            TroopColl = new ObservableCollection<TroopClass>() {
+                new TroopClass("Slugga Boyz", 150),
+                new TroopClass("Shoota Boyz", 130)
+            };
+
+        }
+
+
+
+
+
+
+
+    }
+
+
+    public class TroopClass
+    {
+
+        public TroopClass(string troopName, int troopPoints)
+        {
+            TroopName = troopName;
+            TroopPoints = troopPoints;
+        }
+
+        public string TroopName { get; set; }
+        public int TroopPoints { get; set; }
+    }
+
+
+    public class HQClass
+    {
+        public HQClass(string hQTitle, int hQPoints)
+        {
+            HQTitle = hQTitle;
+            HQPoints = hQPoints;
+        }
+        public string HQTitle { get; set; }
+        public int HQPoints { get; set; }
+    }
+
+}
